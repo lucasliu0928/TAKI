6be3112374f69427766acd0f1314eb5db9250644
JLLucas <39839798+jingliu0928@users.noreply.github.com>
2021-02-11 01:49:18 -0500

    Update Final_Classification_Mortality.R

--- Final_Classification_Mortality.R
+++ Final_Classification_Mortality.R
@@ -1,6 +1,24 @@
 library(pROC)
 library(caret)
 
+code_Label_YN_func <- function(input_df,label_colname){
+  #Recode label as Y for 1, N for 0, cuz for caret package, we cannot use 1 or 0 for outcome
+  
+  input_df[,label_colname][which(input_df[,label_colname] == 1)] <- "Y"
+  input_df[,label_colname][which(input_df[,label_colname] == 0)] <- "N"
+  input_df[,label_colname] <-as.factor(input_df[,label_colname])
+  return(input_df)
+}
+
+
+code_Label_10_func <- function(input_df,label_colname){
+  #Reocde label from YN back to 10 for performance computation
+  input_df[,label_colname] <- as.character(input_df[,label_colname])
+  input_df[,label_colname][which(input_df[,label_colname] == "Y")] <- 1
+  input_df[,label_colname][which(input_df[,label_colname] == "N")] <- 0
+  return(input_df)
+}
+
 create_fold_func <- function(analysis_df){
   #this function returns row index of analysis_df for each fold
   #create 10 folds
@@ -19,113 +37,153 @@ create_fold_func <- function(analysis_df){
   return(Idxes_fold_df)
 }
 
-cv_func <- function(analysis_df,outcome_colname,model_name,validation_df){
-  #get folds info table 
+#Sampling function
+Model_sampling_func <- function(upsample_flag,train_data,label_col_name,seed_num){
+  # upsample_flag <- 0
+  # train_data <- curr_train_data
+  # label_col_name <- outcome_colname
+  # seed_num <- 1
+  
+  #Get label col index
+  label_col_index <- which(colnames(train_data) == label_col_name)
+  
+  #Sampling
+  if(upsample_flag==1){ #upsampling
+    set.seed(seed_num)
+    up_train <- upSample(x = train_data[, -label_col_index],
+                         y = as.factor(train_data[,label_col_name]), yname = label_col_name)  
+    sampled_train_data <- up_train
+
+  }else if(upsample_flag==0){ #downsample
+    set.seed(seed_num)
+    down_train <- downSample(x = train_data[, -label_col_index],
+                             y = as.factor(train_data[,label_col_name]), yname = label_col_name)      
+    sampled_train_data <- down_train
+
+  }else{
+    original_train <- train_data
+    sampled_train_data <- original_train
+  }
+  
+ 
+  
+  return(sampled_train_data)
+}
+
+#Convert prediction probability to prediction class, threshold = 0.5
+get_pred_class_func <- function(predicted_prob){
+  pred_class <- predicted_prob
+  pred_class[which(predicted_prob >=0.5)] <- 1
+  pred_class[which(predicted_prob < 0.5)] <- 0
+  pred_class <- as.factor(pred_class)
+  return(pred_class)
+}
+
+
+cv_func <- function(analysis_df,outcome_colname,model_name,validation_df,upsample_flag,N_sampling){
+  # analysis_df <- feature_df
+  # outcome_colname <- "died_inp"
+  # model_name <- "SVM"
+  # validation_df <- UTSW_feature_df
+  # upsample_flag <- 0
+  # N_sampling <- 5
+  #Get folds indexes table 
   Idxes_fold_df <- create_fold_func(analysis_df)
   
-  
   #code factor as Y for 1, and N for 0, for caret package, we cannot use 1 or 0 for outcome
-  analysis_df[,outcome_colname][which(analysis_df[,outcome_colname] == 1)] <- "Y"
-  analysis_df[,outcome_colname][which(analysis_df[,outcome_colname] == 0)] <- "N"
-  analysis_df[,outcome_colname] <-as.factor(analysis_df[,outcome_colname])
+  #For UK df
+  analysis_df <- code_Label_YN_func(analysis_df,outcome_colname)
   outcome_index <- which(colnames(analysis_df) == outcome_colname)
-  
-  #for utsw validation df
-  validation_df[,outcome_colname][which(validation_df[,outcome_colname] == 1)] <- "Y"
-  validation_df[,outcome_colname][which(validation_df[,outcome_colname] == 0)] <- "N"
-  validation_df[,outcome_colname] <-as.factor(validation_df[,outcome_colname])
-  outcome_index_utsw <- which(colnames(analysis_df) == outcome_colname)
+  #For utsw validation df
+  validation_df <- code_Label_YN_func(validation_df,outcome_colname)
+  outcome_index_utsw <- which(colnames(validation_df) == outcome_colname)
 
-  pred_table_list <- list(NA)
-  pred_table_Validation_list <- list(NA)
-  for (i in 1:10){
+  
+  #Training and prediction
+  All_sampling_results_perFold <- list(NA)
+  All_sampling_results_perFold_validation <- list(NA)
+  for (i in 1:10){ #10 Fold
     curr_test_data <- analysis_df[which(Idxes_fold_df[,"Fold"] == i),]
     curr_train_data <-  analysis_df[which(Idxes_fold_df[,"Fold"] != i),]
     
+    #sampling for traning data
+    sampling_pred_table_list <- list(NA)
+    sampling_pred_table_Validation_list <- list(NA)
+    for (s in 1:N_sampling){
+          print(paste0("Fold",i,": Sampling",s))
+          seed_num <- s*i
+          sampled_train_data <- Model_sampling_func(upsample_flag,curr_train_data,outcome_colname,seed_num)
+      
+          if (model_name == "SVM"){
+            model_svm  <- train(sampled_train_data[,-outcome_index], sampled_train_data[,outcome_index],method='svmPoly' , 
+                                trControl = trainControl("none", classProbs = TRUE),verbose=F) # Support Vector Machines
+            curr_model <- model_svm
+          }else if (model_name == "RF"){
+            model_rf <- train(sampled_train_data[,-outcome_index], sampled_train_data[,outcome_index], method='rf',
+                              trControl = trainControl("none", classProbs = TRUE), verbose=F) # Random Forest
+            curr_model <- model_rf
+          }else if (model_name == "LogReg"){
+            model_logreg <- glm(as.formula(paste0(eval(outcome_colname) ,"~.")), data = sampled_train_data, family = binomial)
+            curr_model <- model_logreg
+          }
+          
+          #prediction probabiliy of test data and validation data
+          if (model_name =="SVM" | model_name =="RF" ){
+              pred_res <- predict(curr_model, newdata = curr_test_data[,-outcome_index],type = "prob")  
+              pred_prob <- pred_res[,"Y"] #use the prob for Y or 1
+              
+              pred_res <- predict(curr_model, newdata = validation_df[,-outcome_index_utsw],type = "prob")  
+              pred_prob_validation <- pred_res[,"Y"] #use the prob for Y or 1
+              
+          }else{
+             pred_res <- predict(curr_model, curr_test_data, type='response')  #do not need exclude col name from test here, it will not use it for prediciton
+             pred_prob <- as.numeric(pred_res)
+             
+             pred_res <- predict(curr_model, validation_df, type='response')  #do not need exclude col name from test here, it will not use it for prediciton
+             pred_prob_validation <- as.numeric(pred_res)
+          }
+          
     
-    if (model_name == "SVM"){
-      model_svm  <- train(curr_train_data[,-outcome_index], curr_train_data[,outcome_index],method='svmRadial' , 
-                          trControl = trainControl("none", classProbs = TRUE),verbose=F) # Support Vector Machines
-      curr_model <- model_svm
-    }else if (model_name == "RF"){
-      model_rf <- train(curr_train_data[,-outcome_index], curr_train_data[,outcome_index], method='rf',
-                        trControl = trainControl("none", classProbs = TRUE), verbose=F) # Random Forest
-      curr_model <- model_rf
-    }else if (model_name == "LogReg"){
-      model_logreg <- glm(as.formula(paste0(eval(outcome_colname) ,"~.")), data = curr_train_data, family = binomial)
-      curr_model <- model_logreg
-    }
-    
-    #prediction of test data and validation data
-    if (model_name =="SVM" | model_name =="RF" ){
-        pred_res <- predict(curr_model, newdata = curr_test_data[,-outcome_index],type = "prob")  
-        pred_prob <- pred_res[,"Y"] #use the prob for Y or 1
+          #Prediction class
+          pred_class <- get_pred_class_func(pred_prob) #testing 
+          pred_class_validation <- get_pred_class_func(pred_prob_validation) #external validation
+          
+          
+          #Code outcome of test back to 1 and 0
+          curr_test_data <- code_Label_10_func(curr_test_data,outcome_colname)
+          validation_df <- code_Label_10_func(validation_df,outcome_colname)
+          
+          #combine pred prob, pred class, acutal label, and fold index
+          #Testing results
+          pred_table <- cbind.data.frame(rownames(curr_test_data),pred_prob,pred_class,as.numeric(curr_test_data[,outcome_colname]),paste0("Fold",i),paste0("S",s))
+          colnames(pred_table) <- c("ID","pred_prob","pred_class","Label","TestFold","TrainingSample_Index")
+          sampling_pred_table_list[[s]] <- pred_table
         
-        pred_res <- predict(curr_model, newdata = validation_df[,-outcome_index_utsw],type = "prob")  
-        pred_prob_validation <- pred_res[,"Y"] #use the prob for Y or 1
-        
-    }else{
-       pred_res <- predict(curr_model, curr_test_data, type='response')  #do not need exclude col name from test here, it will not use it for prediciton
-       pred_prob <- as.numeric(pred_res)
-       
-       pred_res <- predict(curr_model, validation_df, type='response')  #do not need exclude col name from test here, it will not use it for prediciton
-       pred_prob_validation <- as.numeric(pred_res)
-    }
+          #External validation results
+          pred_table_validation <- cbind.data.frame(rownames(validation_df),pred_prob_validation,pred_class_validation,as.numeric(validation_df[,outcome_colname]),paste0("Fold",i),paste0("S",s))
+          colnames(pred_table_validation) <- c("ID","pred_prob","pred_class","Label","TestFold","TrainingSample_Index")
+          sampling_pred_table_Validation_list[[s]] <- pred_table_validation
+    } # N_sampling times sampling
     
-    #get Pred class cutoff = 0.5
-    pred_class <- pred_prob
-    pred_class[which(pred_prob >=0.5)] <- 1
-    pred_class[which(pred_prob < 0.5)] <- 0
-    pred_class <- as.factor(pred_class)
-    
-    #Code outcome of test back to 1 and 0
-    curr_test_data[,outcome_colname] <- as.character(curr_test_data[,outcome_colname])
-    curr_test_data[,outcome_colname][which(curr_test_data[,outcome_colname] == "Y")] <- 1
-    curr_test_data[,outcome_colname][which(curr_test_data[,outcome_colname] == "N")] <- 0
-    
-    #combine pred prob, pred class, acutal label, and fold index
-    pred_table <- cbind.data.frame(pred_prob,pred_class,as.numeric(curr_test_data[,outcome_colname]),paste0("Fold",i))
-    colnames(pred_table) <- c("pred_prob","pred_class","Label","TestFold")
-    pred_table_list[[i]] <- pred_table
-    
-    
-    ####################################################################################################################
-    ### Validation data
-    ####################################################################################################################
-    #validation get Pred class cutoff = 0.5 
-    pred_class_validation <- pred_prob_validation
-    pred_class_validation[which(pred_prob_validation >=0.5)] <- 1
-    pred_class_validation[which(pred_prob_validation < 0.5)] <- 0
-    pred_class_validation <- as.factor(pred_class_validation)
-    
-    #Code outcome of test back to 1 and 0
-    validation_df[,outcome_colname] <- as.character(validation_df[,outcome_colname])
-    validation_df[,outcome_colname][which(validation_df[,outcome_colname] == "Y")] <- 1
-    validation_df[,outcome_colname][which(validation_df[,outcome_colname] == "N")] <- 0
-    
-    #combine pred prob, pred class, acutal label, and fold index
-    pred_table_validation <- cbind.data.frame(pred_prob_validation,pred_class_validation,as.numeric(validation_df[,outcome_colname]),paste0("Fold",i))
-    colnames(pred_table_validation) <- c("pred_prob","pred_class","Label","TestFold")
-    pred_table_Validation_list[[i]] <- pred_table_validation
-    
-    
-    
+    All_sampling_results_perFold[[i]] <- do.call(rbind,sampling_pred_table_list)
+    All_sampling_results_perFold_validation[[i]] <- do.call(rbind,sampling_pred_table_Validation_list)
   }
   
-  final_pred <- do.call(rbind,pred_table_list)
-  final_pred_validation <- do.call(rbind,pred_table_Validation_list)
+  final_pred <- do.call(rbind,All_sampling_results_perFold)
+  final_pred_validation <- do.call(rbind,All_sampling_results_perFold_validation)
   
   return(list(final_pred,final_pred_validation))
 }
 
 
 #User input
+outdir <- "/Users/lucasliu/Desktop/DrChen_Projects/All_AKI_Projects/Other_Project/TAKI_Project/Intermediate_Results/Prediction_results0210/"
+
 UK_dir <- "/Users/lucasliu/Desktop/DrChen_Projects/All_AKI_Projects/Other_Project/TAKI_Project/TAKI_Data/uky/"
 feature_file <- "clinical_model_mortality_norm.csv" #"clinical_model_mortality_wTrajectory_norm.csv"
 outcome_file <- "Old_outcomes.csv"
 outcome_colname <- "died_inp"
-outdir <- "/Users/lucasliu/Desktop/DrChen_Projects/All_AKI_Projects/TAKI_Project/Intermediate_Results/mortality/"
-outfile_pname <- gsub("_mortality_|_norm.csv","",feature_file)
+outfile_pname <- gsub("_mortality|_norm.csv","",feature_file)
 
 
 UTSW_dir <- "/Users/lucasliu/Desktop/DrChen_Projects/All_AKI_Projects/Other_Project/TAKI_Project/TAKI_Data/utsw/"
@@ -171,23 +229,25 @@ validation_df <- UTSW_feature_df
 
 #################################################
 ####### 10-fold cv Training And prediction ####### 
-#################################################
+################################################# 
+upsample_flag <- 0 #ds
+
 # SVM 
-pred_table_SVM <- cv_func(analysis_df,outcome_colname,"SVM",validation_df)
+pred_table_SVM <- cv_func(analysis_df,outcome_colname,"SVM",validation_df,upsample_flag,10)
 UKY_perd_table <- pred_table_SVM[[1]]
 UTSW_perd_table <- pred_table_SVM[[2]]
 write.csv(UKY_perd_table,paste0(outdir,outfile_pname,"_pred_table_SVM_UKY.csv"))
 write.csv(UTSW_perd_table,paste0(outdir,outfile_pname,"_pred_table_SVM_UTSW.csv"))
 
 # RF
-pred_table_RF <- cv_func(analysis_df,outcome_colname,"RF",validation_df)
+pred_table_RF <- cv_func(analysis_df,outcome_colname,"RF",validation_df,upsample_flag,10)
 UKY_perd_table_RF <- pred_table_RF[[1]]
 UTSW_perd_table_RF <- pred_table_RF[[2]]
 write.csv(UKY_perd_table_RF,paste0(outdir,outfile_pname,"_pred_table_RF_UKY.csv"))
 write.csv(UTSW_perd_table_RF,paste0(outdir,outfile_pname,"_pred_table_RF_UTSW.csv"))
 
 # Logreg
-pred_table_Logreg <- cv_func(analysis_df,outcome_colname,"LogReg",validation_df)
+pred_table_Logreg <- cv_func(analysis_df,outcome_colname,"LogReg",validation_df,upsample_flag,10)
 UKY_perd_table_Logreg <- pred_table_Logreg[[1]]
 UTSW_perd_table_Logreg <- pred_table_Logreg[[2]]
 write.csv(UKY_perd_table_Logreg,paste0(outdir,outfile_pname,"_pred_table_Logreg_UKY.csv"))
