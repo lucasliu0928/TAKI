45a50ba22323cf7e538ea090e57a392694ee1734
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-06-04 13:29:17 -0400

    Create 3_Parameters_Statstics.R

---
+++ 3_Parameters_Statstics.R
@@ -0,0 +1,31 @@
+compute_missing_rate <- function(data_input,col_name){
+  #data_input <- UK_df
+  #col_name <- "Albumin"
+  
+  n_missing <- length(which(is.na(data_input[,col_name])==T))
+  perc_missing <- round(n_missing*100/nrow(data_input),2)
+  res <- paste0(n_missing," (",perc_missing, "%)")
+  return(res)
+}
+
+
+data_dir <- "/Volumes/LJL_ExtPro/Data/AKI_Data/Taylors_Data/TAKI_Feature/features_forsubmission/"
+
+UTSW_df <- read.csv(paste0(data_dir,"utsw/base_model_ALL_FEATURES.csv"),stringsAsFactors = F)
+
+UK_df$Albumin
+
+#############################################################
+# UK data
+#############################################################
+#Load UK base data
+UK_df <- read.csv(paste0(data_dir,"uky/base_model_ALL_FEATURES.csv"),stringsAsFactors = F)
+
+#Load UK exclusion
+UK_exclusion_df <- read.csv(paste0(data_dir,"uky/exclusion_criteria._taylor.csv"),stringsAsFactors = F)
+UK_ID_left_df <- UK_exclusion_df[which(rowSums(UK_exclusion_df[,2:ncol(UK_exclusion_df)])==0),] #keep pt without "1" in any of the column
+
+#filter exclusion
+updated_UK_df <- UK_df[which(UK_df[,"STUDY_PATIENT_ID"] %in% UK_ID_left_df[,"STUDY_PATIENT_ID"]),]
+
+compute_missing_rate(updated_UK_df,"Albumin")
