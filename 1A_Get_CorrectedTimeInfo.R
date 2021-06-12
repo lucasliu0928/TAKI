fcad3256c6f8d8ce43151477240f6baa04283949
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-06-11 23:23:15 -0400

    Update 1A_Get_CorrectedTimeInfo.R

--- 1A_Get_CorrectedTimeInfo.R
+++ 1A_Get_CorrectedTimeInfo.R
@@ -33,8 +33,7 @@ get_one_ICU_admission_func <- function(one_hosp_df){
 
 #Raw data dir
 raw_dir <- "/Volumes/LJL_ExtPro/Data/AKI_Data/Taylors_Data/UKY/raw_csv_files/"
-taylor_UK_data_dir <- "/Volumes/LJL_ExtPro/Data/AKI_Data/Taylors_Data/TAKI_Feature/features_candidates/uky/"
-outdir <- "/Volumes/LJL_ExtPro/Data/AKI_Data/Taylors_Data/TAKI_Feature/rawData_processed/uky/"
+outdir <- "/Volumes/LJL_ExtPro/Data/AKI_Data/TAKI_Data_Extracted/uky/"
 
 
 ##########################################################################################
@@ -71,7 +70,7 @@ if (length(idx_ICU_equal_time) > 0 ){
   raw_ADMISSION_INDX_df <- raw_ADMISSION_INDX_df[-idx_ICU_equal_time,]
 }
 
-#7. remove ICU start > hosp end
+#7. remove ICU start > ICU end
 idx_icu_endBeforeStart <- which(raw_ADMISSION_INDX_df[,"ICU_ADMIT_DATE"] > raw_ADMISSION_INDX_df[,"ICU_DISCHARGE_DATE"])
 if (length(idx_icu_endBeforeStart) > 0 ){
   raw_ADMISSION_INDX_df <- raw_ADMISSION_INDX_df[-idx_icu_endBeforeStart,]
@@ -98,7 +97,7 @@ for (j in 1:length(reformat_cols)){
 raw_RENAL_REPLACE_THERAPY_df <- raw_RENAL_REPLACE_THERAPY_df[!duplicated(raw_RENAL_REPLACE_THERAPY_df[,reformat_cols]),]
 
 #4.get CRRT/HD start = CRRT/HD end IDs, exclude them from analysis ID, 
-#because won't be able tocompute RRT time and can not make sure if on RRT during ICU
+#because won't be able to compute RRT time and can not make sure if on RRT during ICU
 idx_CRRT_equal_time <- which(raw_RENAL_REPLACE_THERAPY_df[,"CRRT_START_DATE"] == raw_RENAL_REPLACE_THERAPY_df[,"CRRT_STOP_DATE"])
 idx_CRRT_startBeforeEnd <- which(raw_RENAL_REPLACE_THERAPY_df[,"CRRT_START_DATE"] > raw_RENAL_REPLACE_THERAPY_df[,"CRRT_STOP_DATE"])
 idx_HD_equal_time <- which(raw_RENAL_REPLACE_THERAPY_df[,"HD_START_DATE"] == raw_RENAL_REPLACE_THERAPY_df[,"HD_STOP_DATE"])
@@ -192,53 +191,279 @@ for (i in 1:length(reformat_cols)){
 }
 
 ##########################################################################################
-#Correction
+#Check if patients has ICU, HOSP,RRT dates before correction
+#1: Has the corresponidng dates
+#0: No date
 ##########################################################################################
-#Compute ICU admit - hosp admit (supposed to be >= 0, ICU admit after hosp admit)
-time1 <- ymd_hms(TimeInfo_df[,"First_ICU_ADMIT_DATE"])
-time2 <- ymd_hms(TimeInfo_df[,"HOSP_ADMIT_DATE"])
-TimeInfo_df$ICUAD_minus_HOSPAD_inDays <- difftime(time1,time2,units = "days") #ICU - HOSP
+has_date_df <- check_has_dates(TimeInfo_df)
+table(has_date_df$HAS_HOSP)
+table(has_date_df$HAS_ICU)
+table(has_date_df$HAS_CRRT)
+table(has_date_df$HAS_HD)
 
-#Compute ICU discharge - hosp discharge (supposed to be <= 0, ICU discharge before hosp discharge)
-time1 <- TimeInfo_df[,"First_ICU_DISCHARGE_DATE"]
-time2 <- TimeInfo_df[,"HOSP_DISCHARGE_DATE"]
-TimeInfo_df$ICUDC_minus_HOSPDC_inDays <- difftime(time1,time2,units = "days") #ICU - HOSP
+##########################################################################################
+#Check if patients has Valid ICU, HOSP,RRT dates before correction 
+# if CRRT,HD has overlap
+# If CRRT, HD and ICU in HOSP with 0 hours to extend (exact in)
+# if CRRT in ICU  with 0 hours to extend (exact in)
+##########################################################################################
+# valid_dates_df <- check_valid_dates(TimeInfo_df,0)
+# table(valid_dates_df$CRRT_inHOSP)
 
-##Correct admission time
-#Initilization
-TimeInfo_df$Corrected_HOSP_ADMIT_DATE <- TimeInfo_df[,"HOSP_ADMIT_DATE"]
+##########################################################################################
+#'Time correction for overlapped CRRT and HD dates for patients has both dates
+#'@TODO:  Check function conditions
+##########################################################################################
+has_bothRRT_idxes <- which(has_date_df$HAS_CRRT == 1 & has_date_df$HAS_HD == 1)
+has_bothRRT_IDs <- has_date_df[has_bothRRT_idxes, "STUDY_PATIENT_ID"]
 
-#1.if ICU admit is more than 1 day before HOSP (ICU_admit - HOSP_admit < -1 days), exclude
-idxes_ICUbeforeHOSP_morethan1 <- which(TimeInfo_df[,"ICUAD_minus_HOSPAD_inDays"] < -1) #8
-TimeInfo_df <- TimeInfo_df[-idxes_ICUbeforeHOSP_morethan1,]
+corrected_CRRTHD_df <- as.data.frame(matrix(NA, nrow = length(has_bothRRT_IDs), ncol = 6))
+colnames(corrected_CRRTHD_df) <- c("STUDY_PATIENT_ID", "Updated_CRRT_Start","Updated_CRRT_End",
+                                   "Updated_HD_Start","Updated_HD_End","Overlap_Flag")
+for (i in 1:length(has_bothRRT_IDs)){
+  if (i %% 100 == 0){print(i)}
+  curr_id <- has_bothRRT_IDs[i]
+  
+  #Time df
+  curr_time_df  <- TimeInfo_df[which(TimeInfo_df[,"STUDY_PATIENT_ID"] == curr_id),]
+  curr_CRRT_Start <- curr_time_df[ ,"CRRT_START_DATE"]
+  curr_CRRT_End <- curr_time_df[ ,"CRRT_STOP_DATE"]
+  curr_HD_Start <- curr_time_df[ ,"HD_START_DATE"]
+  curr_HD_End <- curr_time_df[ ,"HD_STOP_DATE"]
+  
+  
+  res <- correct_overlap_CRRTHD(curr_CRRT_Start,curr_CRRT_End,curr_HD_Start,curr_HD_End)
 
-#2. if if ICU admit is less than 1 day before HOSP admit, 
-#make the hosp and ICU  the same by setting hosp admit time = ICU admit time
-idxes_ICUbeforeHOSP_lessthan1 <- which(TimeInfo_df[,"ICUAD_minus_HOSPAD_inDays"] >= -1 &
-                                       TimeInfo_df[,"ICUAD_minus_HOSPAD_inDays"] < 0) #696
-TimeInfo_df[idxes_ICUbeforeHOSP_lessthan1,"Corrected_HOSP_ADMIT_DATE"] <- TimeInfo_df[idxes_ICUbeforeHOSP_lessthan1,"First_ICU_ADMIT_DATE"]
-
-##Correct discharge time
-#Initilization
-TimeInfo_df$Corrected_HOSP_DISCHARGE_DATE <- TimeInfo_df[,"HOSP_DISCHARGE_DATE"]
-
-#1.if ICU discahrge is more than 1 day after HOSP discahrge (ICU_dc- HOSP_dc > 1 days), exclude
-idxes_ICUafterHOSP_morethan1 <- which(TimeInfo_df[,"ICUDC_minus_HOSPDC_inDays"] > 1) #0
-if (length(idxes_ICUafterHOSP_morethan1) > 0){
-TimeInfo_df <- TimeInfo_df[-idxes_ICUafterHOSP_morethan1,]
+  corrected_CRRTHD_df[i,"STUDY_PATIENT_ID"] <- curr_id
+  corrected_CRRTHD_df[i,"Updated_CRRT_Start"] <- as.character(res[,1])
+  corrected_CRRTHD_df[i,"Updated_CRRT_End"] <- as.character(res[,2])
+  corrected_CRRTHD_df[i,"Updated_HD_Start"] <-as.character(res[,3])
+  corrected_CRRTHD_df[i,"Updated_HD_End"] <- as.character(res[,4])
+  corrected_CRRTHD_df[i,"Overlap_Flag"] <- as.character(res[,5])
+  
+  
+  
 }
 
-#2.if if ICU discahrge is less than 1 day after HOSP discahrge, 
-#make the hosp and ICU  the same by setting hosp discahrge time = ICU discahrge time
-idxes_ICUafterHOSP_lessthan1 <- which(TimeInfo_df[,"ICUDC_minus_HOSPDC_inDays"] < 1 &
-                                      TimeInfo_df[,"ICUDC_minus_HOSPDC_inDays"] > 0)  #0
-if (length(idxes_ICUafterHOSP_lessthan1) > 0){
-TimeInfo_df[idxes_ICUafterHOSP_lessthan1,"Corrected_HOSP_DISCHARGE_DATE"] <- TimeInfo_df[idxes_ICUafterHOSP_lessthan1,"First_ICU_DISCHARGE_DATE"]
+table(corrected_CRRTHD_df$Overlap_Flag)
+
+#Store corrected dates to time df
+TimeInfo_df$Updated_CRRT_Start <- NA
+TimeInfo_df$Updated_CRRT_End <- NA
+TimeInfo_df$Updated_HD_Start <- NA
+TimeInfo_df$Updated_HD_End <- NA
+TimeInfo_df$Overlap_Flag <- NA
+
+for (i in 1:nrow(TimeInfo_df)){
+  if (i %% 1000 == 0){print(i)}
+  curr_id <- TimeInfo_df[i,"STUDY_PATIENT_ID"]
+  
+  curr_idxes_inCorrected <- which(corrected_CRRTHD_df[,"STUDY_PATIENT_ID"] == curr_id)
+  if (length(curr_idxes_inCorrected) == 0 ){ #if has not been corrected
+    TimeInfo_df[i,"Updated_CRRT_Start"] <- TimeInfo_df[i,"CRRT_START_DATE"]
+    TimeInfo_df[i,"Updated_CRRT_End"] <- TimeInfo_df[i,"CRRT_STOP_DATE"]
+    TimeInfo_df[i,"Updated_HD_Start"] <- TimeInfo_df[i,"HD_START_DATE"]
+    TimeInfo_df[i,"Updated_HD_End"] <- TimeInfo_df[i,"HD_STOP_DATE"]
+    TimeInfo_df[i,"Overlap_Flag"] <- NA
+  }else {
+    TimeInfo_df[i,"Updated_CRRT_Start"] <- corrected_CRRTHD_df[curr_idxes_inCorrected,"Updated_CRRT_Start"]
+    TimeInfo_df[i,"Updated_CRRT_End"] <- corrected_CRRTHD_df[curr_idxes_inCorrected,"Updated_CRRT_End"]
+    TimeInfo_df[i,"Updated_HD_Start"] <- corrected_CRRTHD_df[curr_idxes_inCorrected,"Updated_HD_Start"]
+    TimeInfo_df[i,"Updated_HD_End"] <- corrected_CRRTHD_df[curr_idxes_inCorrected,"Updated_HD_End"]
+    TimeInfo_df[i,"Overlap_Flag"] <- corrected_CRRTHD_df[curr_idxes_inCorrected,"Overlap_Flag"]
+  } 
+
 }
 
-#check
-which(TimeInfo_df$Corrected_HOSP_ADMIT_DATE > TimeInfo_df$First_ICU_ADMIT_DATE)
-which(TimeInfo_df$Corrected_HOSP_DISCHARGE_DATE < TimeInfo_df$First_ICU_DISCHARGE_DATE)
+
+#Drop old columns
+drop_idxes <- which(colnames(TimeInfo_df) %in% c("CRRT_START_DATE","CRRT_STOP_DATE", 
+                                                  "HD_START_DATE", "HD_STOP_DATE"))
+TimeInfo_df <- TimeInfo_df[,-drop_idxes]
 
 
-write.csv(TimeInfo_df,paste0(outdir,"All_Timeinfo_df.csv"),row.names = F)
+
+##########################################################################################
+#Correction of CRRT,HD, ICU, HOSP
+#1. If CRRT start is before ICU start, but less than 24 hours, set ICU start = CRRT Start; otherwise, exclude pts
+#   If CRRT End is after ICU end, but less than 24 hours, set ICU End = CRRT End; otherwise, exclude 
+
+#2. If min(CRRT/HD/ICU start) is before HOSP start, but less than 24 hours, set HOSP start = min(CRRT/HD/ICU start) ; otherwise, exclude pts
+#   If max(CRRT/HD/ICU end) is after HOSP end, but less than 24 hours, set HOSP End = max(CRRT/HD/ICU end) ; otherwise, exclude 
+
+##########################################################################################
+#1.Corretion of ICU to cover CRRT, Need to correct ICU first , then correct hosp
+TimeInfo_df$Updated_ICU_ADMIT_DATE <- NA
+TimeInfo_df$Updated_ICU_DISCHARGE_DATE <- NA
+TimeInfo_df$Correction_Flag_Start <- NA
+TimeInfo_df$Correction_Flag_End <- NA
+
+for (p in 1:nrow(TimeInfo_df)){
+  if(p %% 1000==0) {
+    # Print on the screen some message
+    cat(paste0("iteration: ", p, "\n"))
+  }
+  curr_id <- TimeInfo_df[p, "STUDY_PATIENT_ID"]
+  curr_time_df  <- TimeInfo_df[p, ]
+  
+  CRRT_Start <- curr_time_df[ ,"Updated_CRRT_Start"]
+  CRRT_End <- curr_time_df[ ,"Updated_CRRT_End"]
+  HD_Start <- curr_time_df[ ,"Updated_HD_Start"]
+  HD_End <- curr_time_df[ ,"Updated_HD_End"]
+  ICU_Start <- curr_time_df[ ,"First_ICU_ADMIT_DATE"]
+  ICU_End <- curr_time_df[ ,"First_ICU_DISCHARGE_DATE"]
+  HOSP_Start <- curr_time_df[,"HOSP_ADMIT_DATE"]
+  HOSP_End <- curr_time_df[,"HOSP_DISCHARGE_DATE"]
+  
+ 
+  if (is.na(CRRT_Start) == T){ #if not on CRRT, then no correction of ICU
+    TimeInfo_df[p,"Updated_ICU_ADMIT_DATE"] <- ICU_Start
+    TimeInfo_df[p,"Updated_ICU_DISCHARGE_DATE"] <- ICU_End
+  }else if (is.na(CRRT_Start) == F & is.na(ICU_Start) == F){  #If on CRRT, check iF CRRT in ICU +- 24 hours
+    CRRT_ICU_Start_Flag <- check_T1start_T2start_status(CRRT_Start,ICU_Start)
+    if (CRRT_ICU_Start_Flag == "T1 starts 24h before T2 starts"){
+      TimeInfo_df[p,"Correction_Flag_Start"] <- "Exclude"
+    }else if (CRRT_ICU_Start_Flag == "T1 starts before T2 starts But Less Than/equals 24h"){
+      TimeInfo_df[p,"Correction_Flag_Start"] <- "Correct"
+      TimeInfo_df[p,"Updated_ICU_ADMIT_DATE"] <- CRRT_Start 
+    }else{
+      TimeInfo_df[p,"Correction_Flag_Start"] <- "Original"
+      TimeInfo_df[p,"Updated_ICU_ADMIT_DATE"] <- ICU_Start #no correction
+    }
+    
+    
+    CRRT_ICU_End_Flag <- check_T1end_T2end_status(CRRT_End,ICU_End)
+    if (CRRT_ICU_End_Flag == "T1 ends 24h after T2 ends"){
+      TimeInfo_df[p,"Correction_Flag_End"] <- "Exclude"
+      
+    }else if(CRRT_ICU_End_Flag == "T1 ends after T2 ends But Less Than/equals 24h") {
+      TimeInfo_df[p,"Correction_Flag_End"] <- "Correct"
+      TimeInfo_df[p,"Updated_ICU_DISCHARGE_DATE"] <- CRRT_End
+      
+    }else{
+      TimeInfo_df[p,"Correction_Flag_End"] <- "Original"
+      TimeInfo_df[p,"Updated_ICU_DISCHARGE_DATE"] <- ICU_End  #no correction
+    }
+  }
+  
+}
+
+#Exclude patients with any one of the start/end flag is exclude
+toexclude_idxes1 <- which(TimeInfo_df$Correction_Flag_Start == "Exclude")
+toexclude_idxes2 <- which(TimeInfo_df$Correction_Flag_End == "Exclude")
+exclude_idxes_all <- unique(c(toexclude_idxes1,toexclude_idxes2))
+TimeInfo_df <- TimeInfo_df[-exclude_idxes_all,]
+
+#Drop old columns
+drop_idxes <- which(colnames(TimeInfo_df) %in% c("First_ICU_ADMIT_DATE","First_ICU_DISCHARGE_DATE","Overlap_Flag","Correction_Flag_Start","Correction_Flag_End"))
+TimeInfo_df <- TimeInfo_df[,-drop_idxes]
+
+##2. Corretion of HOSP to cover ICU/HD/CRRT
+TimeInfo_df$Updated_HOSP_ADMIT_DATE <- NA
+TimeInfo_df$Updated_HOSP_DISCHARGE_DATE <- NA
+TimeInfo_df$Correction_Flag_Start <- NA
+TimeInfo_df$Correction_Flag_End <- NA
+
+for (p in 1:nrow(TimeInfo_df)){
+  if(p %% 1000==0) {
+    # Print on the screen some message
+    cat(paste0("iteration: ", p, "\n"))
+  }
+  curr_id <- TimeInfo_df[p, "STUDY_PATIENT_ID"]
+  curr_time_df  <- TimeInfo_df[p, ]
+  
+  CRRT_Start <- curr_time_df[ ,"Updated_CRRT_Start"]
+  CRRT_End <- curr_time_df[ ,"Updated_CRRT_End"]
+  HD_Start <- curr_time_df[ ,"Updated_HD_Start"]
+  HD_End <- curr_time_df[ ,"Updated_HD_End"]
+  ICU_Start <- curr_time_df[ ,"Updated_ICU_ADMIT_DATE"]
+  ICU_End <- curr_time_df[ ,"Updated_ICU_DISCHARGE_DATE"]
+  HOSP_Start <- curr_time_df[,"HOSP_ADMIT_DATE"]
+  HOSP_End <- curr_time_df[,"HOSP_DISCHARGE_DATE"]
+  
+  #Minimun start time of CRRT ,HD , ICU
+  min_start <- min(c(ymd_hms(CRRT_Start),ymd_hms(HD_Start),ymd_hms(ICU_Start)),na.rm = T)
+  min_start <- reformat_10char_dates_func(min_start)
+  #Maximum end time of CRRT ,HD , ICU
+  max_End <-   max(c(ymd_hms(CRRT_End),ymd_hms(HD_End),ymd_hms(ICU_End)),na.rm = T)
+  max_End <- reformat_10char_dates_func(max_End)
+
+  Start_Flag <- check_T1start_T2start_status(min_start,HOSP_Start)
+  
+  if (Start_Flag == "T1 starts 24h before T2 starts"){
+    TimeInfo_df[p,"Correction_Flag_Start"] <- "Exclude"
+  }else if (Start_Flag == "T1 starts before T2 starts But Less Than/equals 24h"){
+    TimeInfo_df[p,"Correction_Flag_Start"] <- "Correct"
+    TimeInfo_df[p,"Updated_HOSP_ADMIT_DATE"] <- min_start 
+  }else{
+    TimeInfo_df[p,"Correction_Flag_Start"] <- "Original"
+    TimeInfo_df[p,"Updated_HOSP_ADMIT_DATE"] <- HOSP_Start #no correction
+  }
+    
+  End_Flag <- check_T1end_T2end_status(max_End,HOSP_End)
+  
+  if (End_Flag == "T1 ends 24h after T2 ends"){
+    TimeInfo_df[p,"Correction_Flag_End"] <- "Exclude"
+    
+  }else if(End_Flag == "T1 ends after T2 ends But Less Than/equals 24h") {
+    TimeInfo_df[p,"Correction_Flag_End"] <- "Correct"
+    TimeInfo_df[p,"Updated_HOSP_DISCHARGE_DATE"] <- max_End
+    
+  }else{
+    TimeInfo_df[p,"Correction_Flag_End"] <- "Original"
+    TimeInfo_df[p,"Updated_HOSP_DISCHARGE_DATE"] <-  HOSP_End #no correction
+  }
+
+  
+}
+
+#Exclude patients with any one of the start/end flag is exclude
+toexclude_idxes1 <- which(TimeInfo_df$Correction_Flag_Start == "Exclude")
+toexclude_idxes2 <- which(TimeInfo_df$Correction_Flag_End == "Exclude")
+exclude_idxes_all <- unique(c(toexclude_idxes1,toexclude_idxes2))
+TimeInfo_df <- TimeInfo_df[-exclude_idxes_all,]
+
+#Drop old columns
+drop_idxes <- which(colnames(TimeInfo_df) %in% c("HOSP_ADMIT_DATE","HOSP_DISCHARGE_DATE", 
+                                                 "Correction_Flag_Start","Correction_Flag_End"))
+TimeInfo_df <- TimeInfo_df[,-drop_idxes]
+
+##########################################################################################
+# Add decease date IN TimeInfo_df
+##########################################################################################
+#Mortality time df 
+outcome_df <-read.csv(paste0(raw_dir,"OUTCOMES_COMBINED.csv"),stringsAsFactors = F)
+death_df <- outcome_df[which(outcome_df$DECEASED_DATE!=""),] #so this df only contains pts who died
+death_df <- death_df[!duplicated(death_df[,c("STUDY_PATIENT_ID")]),]
+
+TimeInfo_df$DECEASED_DATE <- NA
+for (i in 1: nrow(TimeInfo_df)){
+  curr_id <- TimeInfo_df[i, "STUDY_PATIENT_ID"]
+  curr_death_df <- death_df[which(death_df[,"STUDY_PATIENT_ID"] ==curr_id),]
+  if (nrow(curr_death_df) == 0){
+    TimeInfo_df[i,"DECEASED_DATE"] <- NA
+  }else{
+    TimeInfo_df[i,"DECEASED_DATE"] <- curr_death_df$DECEASED_DATE
+  }
+}
+
+#exclude patients died before HOSP admit 
+exclude_idxes <- which(mdy(TimeInfo_df$DECEASED_DATE) < ymd_hms(TimeInfo_df$Updated_HOSP_ADMIT_DATE))
+updated_TimeInfo_df <- TimeInfo_df[-exclude_idxes,]
+
+write.csv(updated_TimeInfo_df,paste0(outdir,"All_Corrected_Timeinfo.csv"),row.names = F)
+
+
+##########################################################################################
+#Check if patients has Valid ICU, HOSP,RRT dates after correction 
+##########################################################################################
+check_df <- TimeInfo_df
+colnames(check_df)[which(colnames(check_df) %in% c("Updated_CRRT_Start","Updated_CRRT_End",
+                                                   "Updated_HD_Start","Updated_HD_End","Updated_ICU_ADMIT_DATE",
+                                                   "Updated_ICU_DISCHARGE_DATE",
+                                                   "Updated_HOSP_ADMIT_DATE","Updated_HOSP_DISCHARGE_DATE"))] <- c("CRRT_START_DATE","CRRT_STOP_DATE","HD_START_DATE","HD_STOP_DATE",
+                                                                                                                   "First_ICU_ADMIT_DATE","First_ICU_DISCHARGE_DATE","HOSP_ADMIT_DATE","HOSP_DISCHARGE_DATE")
+valid_dates_df <- check_valid_dates(check_df,0)
+
+
+
