fdd211841e32ab4eb3c70d319194f51e8f3710d6
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-06-17 12:54:55 -0400

    Update 1I_Get_ESRD.R

--- 1I_Get_ESRD.R
+++ 1I_Get_ESRD.R
@@ -63,164 +63,142 @@ ESRD_STATUS_df[which(ESRD_STATUS_df == "Y",arr.ind = T)] <- 1
 
 
 ############################################################################################################
-#3. Process USRDS_ESRD.csv to get during and after HOSP, and within 120 following HOSP discharge
-#Use "During"==1 |  "ESRD_ICU120"==1 for ESRD outcome at 120 days (This is equailtant to do curr_esrd_date > curr_hosp_start & curr_esrd_date <= curr_icu_end_plus120)
-#USe "AT" | "Before" for exclusion 
+#3. Process USRDS_ESRD.csv to get during and within 120 following HOSP discharge
 ############################################################################################################
-ESRD_Indicator_df <- as.data.frame(matrix(NA, nrow = length(analysis_ID), ncol = 8))
-colnames(ESRD_Indicator_df) <- c("STUDY_PATIENT_ID","ESRD_BEFORE_HOSP","ESRD_AT_HOSPADMIT","ESRD_DURING_HOSP","ESRD_AFTER_HOSP","ESRD_ICU120","SOURCE","ESRD_Outcome_USRDS2ndWay")
+ESRD_Indicator_df1 <- as.data.frame(matrix(NA, nrow = length(analysis_ID), ncol = 3))
+colnames(ESRD_Indicator_df1) <- c("STUDY_PATIENT_ID","ESRD_DURING_AND_AFTER_HOSP_Within120D","SOURCE")
 for (i in 1:length(analysis_ID)){
   if (i %% 1000 == 0) {print(i)}
   curr_id <- analysis_ID[i]
-  ESRD_Indicator_df[i,"STUDY_PATIENT_ID"] <- curr_id
+  ESRD_Indicator_df1[i,"STUDY_PATIENT_ID"] <- curr_id
   
   #Time info
   curr_time_df <- All_time_df[which(All_time_df[,"STUDY_PATIENT_ID"] == curr_id),]
   curr_hosp_start <- ymd(strsplit(curr_time_df[,"Updated_HOSP_ADMIT_DATE"],split = " ")[[1]][1]) #only get ymd cuz esrd dates has no hms
   curr_hosp_end   <- ymd(strsplit(curr_time_df[,"Updated_HOSP_DISCHARGE_DATE"],split = " ")[[1]][1])
-  curr_icu_end   <- ymd(strsplit(curr_time_df[,"Updated_ICU_DISCHARGE_DATE"],split = " ")[[1]][1])
-  curr_icu_end_plus120 <- curr_icu_end + days(120)
+  curr_hosp_end_plus120 <- curr_hosp_end + days(120)
   
-  
-  #USRD info
+  #Source 1 USRD info
   curr_usrd_df <- USRDS_ESRD_df[which(USRDS_ESRD_df[,"STUDY_PATIENT_ID"] == curr_id),]
   curr_esrd_date <- ymd(curr_usrd_df$ESRD_DATE)
-  if (nrow(curr_usrd_df) != 0){
-    ESRD_Indicator_df[i,"SOURCE"]<- "inUSRD"
-    if (curr_esrd_date > curr_hosp_start & curr_esrd_date <= curr_hosp_end){
-      ESRD_Indicator_df[i,"ESRD_DURING_HOSP"] <- 1
-    }else if (curr_esrd_date > curr_hosp_end){
-      ESRD_Indicator_df[i,"ESRD_AFTER_HOSP"] <- 1
-    }else if (curr_esrd_date < curr_hosp_start) {
-      ESRD_Indicator_df[i,"ESRD_BEFORE_HOSP"] <- 1
-    }else if (curr_esrd_date == curr_hosp_start){
-      ESRD_Indicator_df[i,"ESRD_AT_HOSPADMIT"] <- 1
+  
+  if (nrow(curr_usrd_df) != 0){ #if in USRDs
+    ESRD_Indicator_df1[i,"SOURCE"]<- "in_USRDS"
+    #During and after within 120 days
+    if (curr_esrd_date > curr_hosp_start & curr_esrd_date <= curr_hosp_end_plus120) {
+      ESRD_Indicator_df1[i,"ESRD_DURING_AND_AFTER_HOSP_Within120D"] <- 1
+    }else{
+      ESRD_Indicator_df1[i,"ESRD_DURING_AND_AFTER_HOSP_Within120D"] <- 0
+    }
+
+  }else{
+    ESRD_Indicator_df1[i,"SOURCE"]<- "notin_USRDS"
+    ESRD_Indicator_df1[i,"ESRD_DURING_AND_AFTER_HOSP_Within120D"]<- 0
+  }
+  
+}
+
+table(ESRD_Indicator_df1$ESRD_DURING_AND_AFTER_HOSP_Within120D)
+
+############################################################################################################
+#4. Process ESRD_STATUS.csv to get during
+############################################################################################################
+ESRD_Indicator_df2 <- as.data.frame(matrix(NA, nrow = length(analysis_ID), ncol = 3))
+colnames(ESRD_Indicator_df2) <- c("STUDY_PATIENT_ID","ESRD_DURING","SOURCE")
+for (i in 1:length(analysis_ID)){
+  if (i %% 1000 == 0) {print(i)}
+  curr_id <- analysis_ID[i]
+  ESRD_Indicator_df2[i,"STUDY_PATIENT_ID"] <- curr_id
+  
+  #source 2: status table
+  curr_ESRD_STATUS_df <- ESRD_STATUS_df[which(ESRD_STATUS_df[,"STUDY_PATIENT_ID"] == curr_id),]
+  
+  if (nrow(curr_ESRD_STATUS_df) != 0){ #if in ESRD_STATUS.csv
+    ESRD_Indicator_df2[i,"SOURCE"]<- "in_STATUS_TABLE"
+    if (curr_ESRD_STATUS_df[,"DURING_INDEXED_INDICATOR"] == 1) {
+      ESRD_Indicator_df2[i,"ESRD_DURING"] <- 1
+    }else{
+      ESRD_Indicator_df2[i,"ESRD_DURING"] <- 0
     }
     
-    #another way for outcome
-    if (curr_esrd_date > curr_hosp_start & curr_esrd_date <= curr_icu_end_plus120){
-      ESRD_Indicator_df[i,"ESRD_Outcome_USRDS2ndWay"] <- 1
-    }
-    #within 120 days following ICU discharge
-    if (curr_esrd_date > curr_icu_end & curr_esrd_date <= curr_icu_end_plus120){
-      ESRD_Indicator_df[i,"ESRD_ICU120"] <- 1
-    }
-  }else { #if not in USRD
-    ESRD_Indicator_df[i,"ESRD_BEFORE_HOSP"] <- NA
-    ESRD_Indicator_df[i,"ESRD_AT_HOSPADMIT"] <- NA
-    ESRD_Indicator_df[i,"ESRD_DURING_HOSP"] <- NA
-    ESRD_Indicator_df[i,"ESRD_AFTER_HOSP"] <- NA
-    ESRD_Indicator_df[i,"ESRD_ICU120"] <- NA
-    ESRD_Indicator_df[i,"SOURCE"]<- "notin_USRD"
-  }
-  
-
-}
-
-##########################################################################################
-#4. Process ESRD_STATUS.csv to get esrd info for patients who are not in USRD_ESRD.csv
-#'@TODO: Use "During"==1 | Question: "After" == 1?
-#USe "AT" | "Before" for exclusion 
-##########################################################################################
-notinUSRD_indxes <- which(ESRD_Indicator_df$SOURCE != "inUSRD")
-
-ESRD_Indicator_df$SOURCE2 <- NA
-for (i in 1:length(notinUSRD_indxes)){
-  if (i %% 1000 == 0) {print(i)}
-  curr_idx <- notinUSRD_indxes[i]
-  curr_id <-  ESRD_Indicator_df[curr_idx,"STUDY_PATIENT_ID"]
-  
-  #source 2:
-  curr_ESRD_STATUS_df <- ESRD_STATUS_df[which(ESRD_STATUS_df[,"STUDY_PATIENT_ID"] == curr_id),]
-  if (nrow(curr_ESRD_STATUS_df) != 0){
-    ESRD_Indicator_df[curr_idx,"SOURCE2"]<- "in_STATUS_Table"
-    ESRD_Indicator_df[curr_idx,"ESRD_BEFORE_HOSP"] <- curr_ESRD_STATUS_df[,"BEFORE_INDEXED_INDICATOR"]
-    ESRD_Indicator_df[curr_idx,"ESRD_AT_HOSPADMIT"] <- curr_ESRD_STATUS_df[,"AT_ADMISSION_INDICATOR"]
-    ESRD_Indicator_df[curr_idx,"ESRD_DURING_HOSP"] <- curr_ESRD_STATUS_df[,"DURING_INDEXED_INDICATOR"]
-    ESRD_Indicator_df[curr_idx,"ESRD_AFTER_HOSP"] <- curr_ESRD_STATUS_df[,"AFTER_INDEXED_INDICATOR"]
   }else{
-    ESRD_Indicator_df[curr_idx,"SOURCE2"]<- "notin_STATUS_Table"
+    ESRD_Indicator_df2[i,"SOURCE"]<- "notin_STATUS_TABLE"
+    ESRD_Indicator_df2[i,"ESRD_DURING"]<- 0
   }
+  
 }
 
-
-##########################################################################################
-#5. Add dates for check
-##########################################################################################
-ESRD_Indicator_df$Updated_HOSP_ADMIT_DATE <- NA
-ESRD_Indicator_df$Updated_HOSP_DISCHARGE_DATE <- NA
-ESRD_Indicator_df$Updated_ICU_ADMIT_DATE <- NA
-ESRD_Indicator_df$Updated_ICU_DISCHARGE_DATE <- NA
-ESRD_Indicator_df$Updated_CRRT_Start <- NA
-ESRD_Indicator_df$Updated_CRRT_End <- NA
-ESRD_Indicator_df$Updated_HD_Start <- NA
-ESRD_Indicator_df$Updated_HD_End <- NA
-ESRD_Indicator_df$ICU_DC_PLUS120Days <- NA
-ESRD_Indicator_df$onRRT_Last48hBeforeDischarge <- NA
+table(ESRD_Indicator_df2$ESRD_DURING)
 
 
-for (i in 1:nrow(ESRD_Indicator_df)){
+############################################################################################################
+#5.Combine Two data source to manually check if STATUS agres USRDS
+############################################################################################################
+ESRD_Comb <- as.data.frame(matrix(NA, nrow = length(analysis_ID), ncol = 5))
+colnames(ESRD_Comb) <- c("STUDY_PATIENT_ID","ESRD_DURING_AND_AFTER_HOSP_Within120D_USRDS",
+                                                "ESRD_DURING_STATUS",
+                                                "SOURCE_USRDS","SOURCE_STATUS")
+for (i in 1:length(analysis_ID)){
   if (i %% 1000 == 0) {print(i)}
-  curr_id <- ESRD_Indicator_df[i,"STUDY_PATIENT_ID"]
-  #Time info
-  curr_time_df <- All_time_df[which(All_time_df[,"STUDY_PATIENT_ID"] == curr_id),]
-  ESRD_Indicator_df[i,"Updated_HOSP_ADMIT_DATE"]  <- curr_time_df[,"Updated_HOSP_ADMIT_DATE"] 
-  ESRD_Indicator_df[i,"Updated_HOSP_DISCHARGE_DATE"]    <- curr_time_df[,"Updated_HOSP_DISCHARGE_DATE"]
-  ESRD_Indicator_df[i,"Updated_ICU_ADMIT_DATE"]    <-  curr_time_df[,"Updated_ICU_ADMIT_DATE"]
-  ESRD_Indicator_df[i,"Updated_ICU_DISCHARGE_DATE"]    <-  curr_time_df[,"Updated_ICU_DISCHARGE_DATE"]
+  curr_id <- analysis_ID[i]
+  ESRD_Comb[i,"STUDY_PATIENT_ID"] <- curr_id
   
-  ESRD_Indicator_df[i,"Updated_CRRT_Start"]   <-  curr_time_df[,"Updated_CRRT_Start"]
-  ESRD_Indicator_df[i,"Updated_CRRT_End"]   <-  curr_time_df[,"Updated_CRRT_End"]
+  #source 1: USRDs
+  curr_USRDs <- ESRD_Indicator_df1[which(ESRD_Indicator_df1[,"STUDY_PATIENT_ID"] == curr_id),]
+  ESRD_Comb[i,"ESRD_DURING_AND_AFTER_HOSP_Within120D_USRDS"] <- curr_USRDs[,"ESRD_DURING_AND_AFTER_HOSP_Within120D"]
+  ESRD_Comb[i,"SOURCE_USRDS"] <- curr_USRDs[,"SOURCE"]
   
-  ESRD_Indicator_df[i,"Updated_HD_Start"]   <-  curr_time_df[,"Updated_HD_Start"]
-  ESRD_Indicator_df[i,"Updated_HD_End"]   <-  curr_time_df[,"Updated_HD_End"]
-  
-  ESRD_Indicator_df[i,"ICU_DC_PLUS120Days"] <- as.character(ymd_hms(ESRD_Indicator_df[i,"Updated_ICU_DISCHARGE_DATE"]) + days(120))
-  
-  #on RRT last 48 hours before HOSP discharge
-  curr_hosp_end <- ymd_hms(curr_time_df[,"Updated_HOSP_DISCHARGE_DATE"])
-  curr_hd_end  <- ymd_hms(curr_time_df[,"Updated_HD_End"])
-  curr_crrt_end <- ymd_hms(curr_time_df[,"Updated_CRRT_End"])
-  if (is.na(curr_hd_end) == F | is.na(curr_crrt_end) == F){ #if one of them is not NA
-      curr_max_RRT_Endtime <- max(c(curr_hd_end,curr_crrt_end), na.rm = T)
-      
-      if (curr_max_RRT_Endtime  >= curr_hosp_end - hours(48)){
-        ESRD_Indicator_df[i,"onRRT_Last48hBeforeDischarge"] <- 1
-      }
-  }
+  #source 2: status table
+  curr_status_tb <- ESRD_Indicator_df2[which(ESRD_Indicator_df2[,"STUDY_PATIENT_ID"] == curr_id),]
+  ESRD_Comb[i,"ESRD_DURING_STATUS"] <- curr_status_tb[,"ESRD_DURING"]
+  ESRD_Comb[i,"SOURCE_STATUS"] <- curr_status_tb[,"SOURCE"]
   
 }
 
-write.csv(ESRD_Indicator_df,paste0(outdir,"ESRD.csv"),row.names=FALSE)
+USRD_DuringAfterWithin120D <- ESRD_Comb[,"ESRD_DURING_AND_AFTER_HOSP_Within120D_USRDS"]
+STATUS_During <- ESRD_Comb[,"ESRD_DURING_STATUS"]
+table(USRD_DuringAfterWithin120D,STATUS_During)
 
-
-##########################################################################################
-# Check ESRD
-##########################################################################################
-#1. Apply exclusion
-## 
-ESRD_Indicator_df <-read.csv(paste0(outdir,"ESRD.csv"),stringsAsFactors = F)
-#In USRDs has outcome= 1,  on RRT last 48 hours
-length(which(ESRD_Indicator_df[,"SOURCE"] == "inUSRD" & 
-             ESRD_Indicator_df[,"ESRD_Outcome_USRDS2ndWay"] == 1 & 
-             ESRD_Indicator_df[,"onRRT_Last48hBeforeDischarge"]==1))
-#In USRDs has outcome =0, on RRT last 48 hours 
-length(which(ESRD_Indicator_df[,"SOURCE"] == "inUSRD" & 
-             is.na(ESRD_Indicator_df[,"ESRD_Outcome_USRDS2ndWay"]) == T & 
-             ESRD_Indicator_df[,"onRRT_Last48hBeforeDischarge"]==1))
-
-###
-DuringFlag1_IDs <- ESRD_STATUS_df$STUDY_PATIENT_ID[which(ESRD_STATUS_df[,"DURING_INDEXED_INDICATOR"] == 1)]
-
-#in USRD and has outcome yes ID
-inUSRDYES_IDs <- ESRD_Indicator_df$STUDY_PATIENT_ID[which(ESRD_Indicator_df[,"SOURCE"] == "inUSRD" & 
-                         ESRD_Indicator_df[,"ESRD_Outcome_USRDS2ndWay"] == 1 & 
-                         ESRD_Indicator_df[,"onRRT_Last48hBeforeDischarge"]==1)]
-#in USRD and has outcome no ID
-inUSRDNO_IDs <- ESRD_Indicator_df$STUDY_PATIENT_ID[which(ESRD_Indicator_df[,"SOURCE"] == "inUSRD" & 
-                                                           is.na(ESRD_Indicator_df[,"ESRD_Outcome_USRDS2ndWay"]) == T & 
-                                                           ESRD_Indicator_df[,"onRRT_Last48hBeforeDischarge"]==1)]
+############################################################################################################
+##6. Add onRRT_last48hours flag to check
+#onRRT last 48hours supposed to be have a during flag 
+############################################################################################################
+ESRD_Comb$onRRT_Last48h <- NA
+for (i in 1:nrow(ESRD_Comb)){
+  if (i %% 1000 == 0) {print(i)}
+  curr_id <- ESRD_Comb[i,"STUDY_PATIENT_ID"]
   
+  #on RRT last 48h flag
+  curr_time_df <- All_time_df[which(All_time_df[,"STUDY_PATIENT_ID"] == curr_id),]
+  curr_onRRT48_flag  <- curr_time_df[,"onRRT_Last48hBeforeDischarge"] 
 
-length(which(inUSRDYES_IDs %in% DuringFlag1_IDs))
-length(which(inUSRDNO_IDs %in% DuringFlag1_IDs))
+  ESRD_Comb[i,"onRRT_Last48h"] <- curr_onRRT48_flag
+
+}
+
+#N of pts who has USRDS no, status no,and on RRT last 48h
+usrds0_status0_onRRT48_df <- ESRD_Comb[which(ESRD_Comb[,"ESRD_DURING_AND_AFTER_HOSP_Within120D_USRDS"] == 0 &
+                                          ESRD_Comb[,"ESRD_DURING_STATUS"]== 0 &
+                                          ESRD_Comb[,"onRRT_Last48h"]==1),]
+nrow(usrds0_status0_onRRT48_df)
+#N of pts who has USRDS no, status yes, and on RRT last 48h
+usrds0_status1_onRRT48_df <- ESRD_Comb[which(ESRD_Comb[,"ESRD_DURING_AND_AFTER_HOSP_Within120D_USRDS"] == 0 &
+                                            ESRD_Comb[,"ESRD_DURING_STATUS"]== 1 &
+                                            ESRD_Comb[,"onRRT_Last48h"]==1),]
+nrow(usrds0_status1_onRRT48_df)
+
+#N of pts who has USRDS yes, status no, and on RRT last 48h
+usrds1_status0_onRRT48_df <- ESRD_Comb[which(ESRD_Comb[,"ESRD_DURING_AND_AFTER_HOSP_Within120D_USRDS"] == 1 &
+                                               ESRD_Comb[,"ESRD_DURING_STATUS"]== 0 &
+                                               ESRD_Comb[,"onRRT_Last48h"]==1),]
+nrow(usrds1_status0_onRRT48_df)
+
+
+#N of pts who has USRDS yes, status yes, and on RRT last 48h
+usrds1_status1_onRRT48_df <- ESRD_Comb[which(ESRD_Comb[,"ESRD_DURING_AND_AFTER_HOSP_Within120D_USRDS"] == 1 &
+                                               ESRD_Comb[,"ESRD_DURING_STATUS"]== 1 &
+                                               ESRD_Comb[,"onRRT_Last48h"]==1),]
+nrow(usrds1_status1_onRRT48_df)
+
+#write.csv(ESRD_Comb,paste0(outdir,"ESRD.csv"),row.names=FALSE)
+
