08d7eac0f57f958764a8c69f06a240b982a3be43
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-06-23 00:58:06 -0400

    Update 1L_Get_Labs.R

--- 1L_Get_Labs.R
+++ 1L_Get_Labs.R
@@ -8,46 +8,18 @@ outdir <- "/Volumes/LJL_ExtPro/Data/AKI_Data/TAKI_Data_Extracted/uky/"
 ##########################################################################################
 #1. Load data
 ##########################################################################################
-#1. Corrected Time df for analysis ID
+#1. Analysis Id after exclusion
+analysis_ID_df <-read.csv(paste0(outdir,"Final_Analysis_ID.csv"),stringsAsFactors = F)
+analysis_ID <- unique(analysis_ID_df[,"STUDY_PATIENT_ID"]) #7354
+
+#2. Corrected Time df 
 All_time_df <-read.csv(paste0(outdir,"All_Corrected_Timeinfo.csv"),stringsAsFactors = F)
+All_time_df <- All_time_df[which(All_time_df$STUDY_PATIENT_ID %in% analysis_ID),] #filter for anlaysis Id only
 
-##########################################################################################
-#2. Analysis Id for pts has corrected HOSP ADMISSION time
-##########################################################################################
-analysis_ID <- unique(All_time_df[,"STUDY_PATIENT_ID"])
-
-##########################################################################################
 #3.Load Labs 
-##########################################################################################
 raw_LAB1_df <- read.csv(paste0(raw_dir,"LABS_SET1.csv"),stringsAsFactors = F)
 raw_LAB2_df <- read.csv(paste0(raw_dir,"LABS_SET2.csv"),stringsAsFactors = F)
 
-##########################################################################################
-#4.Get ICU stays in terms of D0, D1, D2, D3
-##########################################################################################
-ICU_D0_D3_df <- as.data.frame(matrix(NA, nrow = length(analysis_ID) ,ncol = 2))
-colnames(ICU_D0_D3_df) <- c("STUDY_PATIENT_ID", "ICU_Stays_inDays")
-for (i in 1:length(analysis_ID)){
-  if (i %% 1000 ==0){print(i)}
-  
-  curr_id <- analysis_ID[i]
-  ICU_D0_D3_df[i,"STUDY_PATIENT_ID"] <- curr_id
-
-  #Time info
-  curr_time_df <- All_time_df[which(All_time_df[,"STUDY_PATIENT_ID"] == curr_id),]
-  curr_icu_start <- ymd_hms(curr_time_df[,"Updated_ICU_ADMIT_DATE"])
-  curr_icu_end <- ymd_hms(curr_time_df[,"Updated_ICU_DISCHARGE_DATE"])
-  #Get ICU D0 to D3 start and end time
-  curr_ICU_D0D3_time_df <- get_ICUD0_D3_dates_func(curr_icu_start,curr_icu_end)
-  
-  #non NA days
-  non_NA_idxes <-   which(is.na(curr_ICU_D0D3_time_df[,"Day_start"]) == F)
-  non_NA_Days <-  curr_ICU_D0D3_time_df[non_NA_idxes,"Day"]
-  ICU_D0_D3_df[i,c("ICU_Stays_inDays")] <- paste0(paste0("D",non_NA_Days),collapse = "$$")
-
-}
-
-write.csv(ICU_D0_D3_df,paste0(outdir,"ICU_D0toD3_Days_df.csv"))
 
 ##########################################################################################
 #Load UK raw LABS_SET1
