74fc6bd614d3a87d349017d48e7480eb5e5b4f85
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-06-23 05:56:50 -0400

    Create 1N_Get_Ht_Wt_Resp_FiO2_BMI.R

---
+++ 1N_Get_Ht_Wt_Resp_FiO2_BMI.R
@@ -0,0 +1,110 @@
+library(lubridate)
+source("TAKI_Ultility.R")
+
+#Raw data dir
+raw_dir <- "/Volumes/LJL_ExtPro/Data/AKI_Data/Taylors_Data/UKY/raw_csv_files/"
+outdir <- "/Volumes/LJL_ExtPro/Data/AKI_Data/TAKI_Data_Extracted/uky/"
+
+##########################################################################################
+#1. Load data
+##########################################################################################
+#1. Analysis Id after exclusion
+analysis_ID_df <-read.csv(paste0(outdir,"Final_Analysis_ID.csv"),stringsAsFactors = F)
+analysis_ID <- unique(analysis_ID_df[,"STUDY_PATIENT_ID"]) #7354
+
+#2. Corrected Time df 
+All_time_df <-read.csv(paste0(outdir,"All_Corrected_Timeinfo.csv"),stringsAsFactors = F)
+All_time_df <- All_time_df[which(All_time_df$STUDY_PATIENT_ID %in% analysis_ID),] #filter for anlaysis Id only
+
+#3.Load raw data
+raw_CLINICAL_OTHERS_df <- read.csv(paste0(raw_dir,"CLINICAL_OTHERS.csv"),stringsAsFactors = F)
+
+
+##########################################################################################
+#Load UK raw CLINICAL_OTHERS
+#Features to extract :  1. Height
+#                       2. Weight
+#                       3. Respiration D1 Low/High
+#                       4. FIO2 D1 Low/High
+#                       5. BMI
+#Steps: 1. Get raw available values
+#       2. Filter out values if patient not in ICU on that day
+#       3. Remove outlier values
+#       4. Compute missing 
+#       5. impute with median
+##########################################################################################
+#Get Features
+height <- get_raw_var_values_1option_func(raw_CLINICAL_OTHERS_df,analysis_ID,"HEIGHT_CM","HEIGHT_CM_VALUE")
+#comvert to meters
+height$HEIGHT_Meters <-  height$HEIGHT_CM/100
+height <- height[,-which(colnames(height) == "HEIGHT_CM")]
+
+weight <- get_raw_var_values_1option_func(raw_CLINICAL_OTHERS_df,analysis_ID,"INITIAL_WEIGHT_KG","INITIAL_WEIGHT_KG")
+
+resp_d1_Low <- get_raw_var_values_1option_func(raw_CLINICAL_OTHERS_df,analysis_ID,"RESP_RATE_D1_LOW","RESP_RATE_D1_LOW_VALUE")
+resp_d1_High <- get_raw_var_values_1option_func(raw_CLINICAL_OTHERS_df,analysis_ID,"RESP_RATE_D1_HIGH","RESP_RATE_D1_HIGH_VALUE")
+
+fio2_d1_Low <- get_raw_var_values_1option_func(raw_CLINICAL_OTHERS_df,analysis_ID,"FI02_D1_LOW","FI02_D1_LOW_VALUE")
+fio2_d1_High <- get_raw_var_values_1option_func(raw_CLINICAL_OTHERS_df,analysis_ID,"FI02_D1_HIGH","FI02_D1_HIGH_VALUE")
+
+#All features except height,weight since these two has no D0,D1 in names, do not have to filter in the following step
+All_ClinicalOthers_df <- cbind(resp_d1_Low,resp_d1_High,fio2_d1_Low,fio2_d1_High)
+All_ClinicalOthers_df <- All_ClinicalOthers_df[,-which(colnames(All_ClinicalOthers_df) == "STUDY_PATIENT_ID")[-1]] #remove redudant ID columns except the 1st one
+
+#2.code the corresponding faetuere value as NA If patient does not have in ICU on that day 
+updated_ClinicalOthers_df <- remove_featureValue(All_ClinicalOthers_df,All_time_df)
+table(updated_ClinicalOthers_df$Excluded_Feature) #0
+
+#3. Add height and weight back 
+All_ClinicalOthers_df <- cbind(All_ClinicalOthers_df,height,weight)
+All_ClinicalOthers_df <- All_ClinicalOthers_df[,-which(colnames(All_ClinicalOthers_df) == "STUDY_PATIENT_ID")[-1]] #remove redudant ID columns except the 1st one
+
+#3.Remove outlier
+#FI02_D1_LOW
+updated_ClinicalOthers_dfOutlierExcluded <- remove_values_byValue(All_ClinicalOthers_df,"FI02_D1_LOW",21,"Less Than")
+updated_ClinicalOthers_dfOutlierExcluded <- remove_values_byValue(updated_ClinicalOthers_dfOutlierExcluded,"FI02_D1_LOW",100,"Greater Than")
+
+#FI02_D1_HIGH
+updated_ClinicalOthers_dfOutlierExcluded <- remove_values_byValue(updated_ClinicalOthers_dfOutlierExcluded,"FI02_D1_HIGH",21,"Less Than")
+updated_ClinicalOthers_dfOutlierExcluded <- remove_values_byValue(updated_ClinicalOthers_dfOutlierExcluded,"FI02_D1_HIGH",100,"Greater Than")
+
+#RESP_RATE_D1_LOW
+updated_ClinicalOthers_dfOutlierExcluded <- remove_values_byValue(updated_ClinicalOthers_dfOutlierExcluded,"RESP_RATE_D1_LOW",5,"Less Than")
+updated_ClinicalOthers_dfOutlierExcluded <- remove_values_byValue(updated_ClinicalOthers_dfOutlierExcluded,"RESP_RATE_D1_LOW",70,"Greater Than")
+
+#RESP_RATE_D1_HIGH
+updated_ClinicalOthers_dfOutlierExcluded <- remove_values_byValue(updated_ClinicalOthers_dfOutlierExcluded,"RESP_RATE_D1_HIGH",5,"Less Than")
+updated_ClinicalOthers_dfOutlierExcluded <- remove_values_byValue(updated_ClinicalOthers_dfOutlierExcluded,"RESP_RATE_D1_HIGH",70,"Greater Than")
+
+#HEIGHT_Meters
+updated_ClinicalOthers_dfOutlierExcluded <- remove_outlier_BOTOrTOP_5perc(updated_ClinicalOthers_dfOutlierExcluded,"HEIGHT_Meters","BOTTOM and TOP")
+
+#INITIAL_WEIGHT_KG
+updated_ClinicalOthers_dfOutlierExcluded <- remove_values_byValue(updated_ClinicalOthers_dfOutlierExcluded,"INITIAL_WEIGHT_KG",30,"Less Than")
+updated_ClinicalOthers_dfOutlierExcluded <- remove_values_byValue(updated_ClinicalOthers_dfOutlierExcluded,"INITIAL_WEIGHT_KG",200,"Greater Than")
+
+#4. Compute missing
+feature_columns <-  c("FI02_D1_LOW", "FI02_D1_HIGH","RESP_RATE_D1_LOW", "RESP_RATE_D1_HIGH",
+                      "HEIGHT_Meters","INITIAL_WEIGHT_KG")
+missing_table <- get_missing_rate_table(updated_ClinicalOthers_dfOutlierExcluded,feature_columns)
+missing_table
+
+#5.imputation median
+Final_ClinicalOthers_df <- median_imputation_func(updated_ClinicalOthers_dfOutlierExcluded,feature_columns)
+missing_table2 <- get_missing_rate_table(Final_ClinicalOthers_df,feature_columns)
+missing_table2
+
+
+##########################################################################################
+# Compute BMI and add to Final_ClinicalOthers_df
+##########################################################################################
+Final_ClinicalOthers_df$BMI <- NA
+for (i in 1:nrow(Final_ClinicalOthers_df)){
+  curr_wt <- Final_ClinicalOthers_df[i,"INITIAL_WEIGHT_KG"]
+  curr_ht <- Final_ClinicalOthers_df[i,"HEIGHT_Meters"]
+
+  curr_BMI <- curr_wt/(curr_ht^2)
+  Final_ClinicalOthers_df[i,"BMI"] <- curr_BMI
+}
+
+write.csv(Final_ClinicalOthers_df,paste0(outdir,"All_HT_WT_RESP_FIO2_df.csv"),row.names = F)
