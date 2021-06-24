68f20e961031366f680546c35012a33413da46f6
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-06-23 23:55:03 -0400

    Update 1O_Get_ECMO_IABP_MV_VAD.R

--- 1O_Get_ECMO_IABP_MV_VAD.R
+++ 1O_Get_ECMO_IABP_MV_VAD.R
@@ -30,8 +30,6 @@ raw_ORGANSUPP_VENT_df <- read.csv(paste0(raw_dir,"ORGANSUPP_VENT.csv"),stringsAs
 raw_ORGANSUPP_VAD_df <- read.csv(paste0(raw_dir,"ORGANSUPP_VAD.csv"),stringsAsFactors = F)
 
 
-intersect(raw_ORGANSUPP_ECMO_df$STUDY_PATIENT_ID,All_time_df$STUDY_PATIENT_ID)
-
 ##########################################################################################
 #Features to extract :  1. ECMO in ICU D0-D3
 #                       2. IABP in ICU D0-D3
@@ -39,48 +37,6 @@ intersect(raw_ORGANSUPP_ECMO_df$STUDY_PATIENT_ID,All_time_df$STUDY_PATIENT_ID)
 #                       4. VAD in ICU D0-D3
 #                       5. Mechanical Hemodynamic Support: Use of VAD, IABP, or ECMO in ICU D0-D3
 ##########################################################################################
-get_onMachine_flag_ICUD0_D3 <- function(machine_df,time_df,pt_id,start_t_col,stop_t_col){
-  # machine_df <- raw_ORGANSUPP_ECMO_df
-  # pt_id <- curr_id
-  # start_t_col <- "ECMO_START_DATE"
-  # stop_t_col <- "ECMO_STOP_DATE"
-  # time_df <- All_time_df 
-  #ICU start
-  curr_time_df <-  time_df[which(time_df[,"STUDY_PATIENT_ID"] == pt_id),]
-  curr_icu_start <- ymd_hms(curr_time_df[,"Updated_ICU_ADMIT_DATE"])
-  
-  #Get actual days/times in ICU D0-D3
-  #it could be ICU end time (e.g, if ICU stays < 3 days) or the end of ICU D3
-  curr_actual_ICU_time_idxes <- which(colnames(curr_time_df) %in% c("Actual_D0_End","Actual_D1_End","Actual_D2_End","Actual_D3_End"))
-  curr_actual_ICU_time <- curr_time_df[,curr_actual_ICU_time_idxes]
-  curr_last_ICU_time <- max(ymd_hms(curr_actual_ICU_time),na.rm = T)
-  
-  ICU_interval <-interval(curr_icu_start,curr_last_ICU_time)
-  
-  
-  curr_df <- machine_df[which(machine_df[,"STUDY_PATIENT_ID"] == pt_id),]
-  if (nrow(curr_df) !=0 ){ #if pt is ever on machine 
-    #get on machine start and end time 
-    curr_machine_start <- ymd_hms(curr_df[,start_t_col])
-    curr_machine_end <-  ymd_hms(curr_df[,stop_t_col])
-    
-    machine_interval <-interval(curr_machine_start,curr_machine_end)
-    
-    if(int_overlaps(ICU_interval, machine_interval)==T){
-      on_flag <- 1 
-      
-    }else{
-      on_flag <- 0 
-    }
-    
-  }else{
-    on_flag <- 0 
-  }
-  
-  return(on_flag)
-}
-
-#362 ID on
 ECMO_IABP_MV_VAD_df <- as.data.frame(matrix(NA, nrow = length(analysis_ID), ncol = 6))
 colnames(ECMO_IABP_MV_VAD_df) <- c("STUDY_PATIENT_ID", "ECMO_ICUD0toD3","IABP_ICUD0toD3",
                                    "MV_ICUD0toD3","VAD_ICUD0toD3","MechanicalHemodynamicSupp_ICUD0toD3")
@@ -101,3 +57,12 @@ for (i in 1: length(analysis_ID)){
     ECMO_IABP_MV_VAD_df[i,"MechanicalHemodynamicSupp_ICUD0toD3"] <- 0
   }
 }
+
+
+#4. Compute missing
+feature_columns <-  c("ECMO_ICUD0toD3", "IABP_ICUD0toD3","MV_ICUD0toD3", "VAD_ICUD0toD3",
+                      "MechanicalHemodynamicSupp_ICUD0toD3")
+missing_table <- get_missing_rate_table(ECMO_IABP_MV_VAD_df,feature_columns)
+missing_table
+
+write.csv(ECMO_IABP_MV_VAD_df,paste0(outdir,"All_ECMO_IABP_MV_VAD_ICUD0toD3.csv"),row.names = F)
