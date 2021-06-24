cce3448c863ef29387299e6cec4ac710db64e6d7
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-06-23 23:55:02 -0400

    Create 1P_Get_onRRT_ICUD0toD3.R

---
+++ 1P_Get_onRRT_ICUD0toD3.R
@@ -0,0 +1,53 @@
+library(lubridate)
+source("TAKI_Ultility.R")
+
+#Raw data dir
+raw_dir <- "/Volumes/LJL_ExtPro/Data/AKI_Data/Taylors_Data/UKY/raw_csv_files/"
+outdir <- "/Volumes/LJL_ExtPro/Data/AKI_Data/TAKI_Data_Extracted/uky/"
+
+##########################################################################################
+#1. Load data
+##########################################################################################
+#1. Analysis Id after exclusion
+analysis_ID_df <-read.csv(paste0(outdir,"Final_Analysis_ID.csv"),stringsAsFactors = F)
+analysis_ID <- unique(analysis_ID_df[,"STUDY_PATIENT_ID"]) #7354
+
+#2. Corrected Time df 
+All_time_df <-read.csv(paste0(outdir,"All_Corrected_Timeinfo.csv"),stringsAsFactors = F)
+All_time_df <- All_time_df[which(All_time_df$STUDY_PATIENT_ID %in% analysis_ID),] #filter for anlaysis Id only
+
+#on RRT alll time df
+onHD_df <- All_time_df[which(is.na(All_time_df$Updated_HD_Start)==F),]
+onCRRT_df <- All_time_df[which(is.na(All_time_df$Updated_CRRT_Start)==F),]
+
+##########################################################################################
+#Features to extract :  1. no RRT                                  (0)
+#                       1. HD only in ICU D0-D3                    (1)
+#                       2. CRRT with/with out HD in ICU D0-D3      (2)
+##########################################################################################
+onRRT_df <- as.data.frame(matrix(NA, nrow = length(analysis_ID), ncol = 2))
+colnames(onRRT_df) <- c("STUDY_PATIENT_ID", "onRRT_ICUD0toD3")
+for (i in 1: length(analysis_ID)){
+  if (i %% 1000== 0){print(i)}
+  curr_id <- analysis_ID[i]
+  onRRT_df[i,"STUDY_PATIENT_ID"] <- curr_id
+  on_HD_ICUD0toD3 <- get_onMachine_flag_ICUD0_D3(onHD_df,All_time_df,curr_id,"Updated_HD_Start","Updated_HD_End")
+  on_CRRT_ICUD0toD3 <- get_onMachine_flag_ICUD0_D3(onCRRT_df,All_time_df,curr_id,"Updated_CRRT_Start","Updated_CRRT_End")
+ 
+  if (on_HD_ICUD0toD3 == 1 &  on_CRRT_ICUD0toD3 == 0){
+    onRRT_df[i,"onRRT_ICUD0toD3"] <- 1  #HD only
+  }else if( (on_HD_ICUD0toD3 == 0 | on_HD_ICUD0toD3 == 1) &  on_CRRT_ICUD0toD3 == 1) {
+    onRRT_df[i,"onRRT_ICUD0toD3"] <- 2 #CRRT with or without HD
+  }else if (on_HD_ICUD0toD3 == 0 &  on_CRRT_ICUD0toD3 == 0){
+    onRRT_df[i,"onRRT_ICUD0toD3"] <- 0 #none
+  }
+}
+
+table(onRRT_df$onRRT_ICUD0toD3)
+
+#4. Compute missing
+feature_columns <-  c("onRRT_ICUD0toD3")
+missing_table <- get_missing_rate_table(onRRT_df,feature_columns)
+missing_table
+
+write.csv(onRRT_df,paste0(outdir,"All_onRRT_ICUD0toD3.csv"),row.names = F)
