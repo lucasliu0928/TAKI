8f805edad19a84e6023d21195b9acb319f518950
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-07-14 22:16:51 -0400

    Create 3D_Get_Avg_PredictionRisk.R

---
+++ 3D_Get_Avg_PredictionRisk.R
@@ -0,0 +1,41 @@
+source("TAKI_Ultility.R")
+
+proj_dir  <- "/Users/lucasliu/Desktop/DrChen_Projects/All_AKI_Projects/Other_Project/TAKI_Project/Intermediate_Results/Prediction_results0708/"
+
+################################################################################################## 
+######                         Mortality                                            ############## 
+################################################################################################## 
+#1. UK
+UK_mortality_dir <- paste0(proj_dir,"CV_performance/mortality/")
+method_name <- "RF"
+#1. Load pred table
+pred_df <- read.csv(paste0(UK_mortality_dir, "SelectedClinicalFeature/Prediction_",method_name,".csv"),stringsAsFactors = F)
+
+#2.Compute avg pred risk
+risk_mortality_UK <- get_avg_pred_func(pred_df)
+write.csv(risk_mortality_UK,paste0(UK_mortality_dir,"UK_Mortality_AVG_Pred_Risk_",method_name,".csv"))
+
+#3.Count risk category
+risk_mortality_UK_count <- count_risk_category(risk_mortality_UK)
+write.csv(risk_mortality_UK_count,paste0(UK_mortality_dir,"UK_Mortality_Risk_Catogory_",method_name,".csv"))
+
+#2.UTSW
+
+################################################################################################## 
+############## MAKE ############## 
+################################################################################################## 
+#1. UK
+UK_MAKE_dir <- paste0(proj_dir,"CV_performance/make120_drop50/")
+method_name <- "RF"
+#1. Load pred table
+pred_df <- read.csv(paste0(UK_MAKE_dir, "SelectedClinicalFeature/Prediction_",method_name,".csv"),stringsAsFactors = F)
+
+#2.Compute avg pred risk
+risk_make_UK <- get_avg_pred_func(pred_df)
+write.csv(risk_make_UK,paste0(UK_MAKE_dir,"UK_MAKE_AVG_Pred_Risk_",method_name,".csv"))
+
+#3.Count risk category
+risk_make_UK_count <- count_risk_category(risk_make_UK)
+write.csv(risk_make_UK_count,paste0(UK_MAKE_dir,"UK_MAKE_Risk_Catogory_",method_name,".csv"))
+
+#UTSW
