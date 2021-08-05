152c350d10911d6c276fdd4cf8cbc6e11e802d8e
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-08-05 17:18:14 -0400

    Update 3A_CrossValidation.R

--- 3A_CrossValidation.R
+++ 3A_CrossValidation.R
@@ -14,62 +14,14 @@ library(caret)
 data_dir <- "/Volumes/LJL_ExtPro/Data/AKI_Data/TAKI_Data_Extracted/uky/Model_Feature_Outcome/"
 
 #out dir
-out_dir <- "/Users/lucasliu/Desktop/DrChen_Projects/All_AKI_Projects/Other_Project/TAKI_Project/Intermediate_Results/Prediction_results0708/CV_performance/"
+out_dir <- "/Users/lucasliu/Desktop/DrChen_Projects/All_AKI_Projects/Other_Project/TAKI_Project/Intermediate_Results/Prediction_results0805/CV_performance/"
 #feature file and outcome file names
 outcome_file <- "All_outcome.csv"
 
 
 
-
 ####################################################################################### 
-######                           Mortality Prediction   1                   ############
-#feature file: 1. All_Feature_imputed_normed.csv, 
-#Outcome file: All_outcome.csv
-####################################################################################### 
-#1.All_Feature_imputed_normed.csv
-feature_file <- c("All_Feature_imputed_normed.csv")
-
-#Outdir for mortality
-outdir1 <- paste0(out_dir,"mortality/AllClinicalFeature/")
-
-#Outcome column name
-outcome_colname <- "Death_inHOSP"
-
-#1.Get model data
-model_data <- construct_model_data_func(data_dir,feature_file,outcome_file,outcome_colname)
-table(model_data$Death_inHOSP)
-colnames(model_data)
-
-#2.CV
-upsample_flag <- 0
-N_sampling <- 10
-NFolds <- 10
-model_name_list <- c("SVM","RF","LogReg","XGB")
-for (m in 1:length(model_name_list)){
-  model_name <- model_name_list[m]
-  #CV
-  cv_res <- cv2_func(model_data,outcome_colname,model_name,upsample_flag,N_sampling,NFolds)
-  final_pred <- cv_res[[1]]
-  write.csv(final_pred, paste0(outdir1,"Prediction_", model_name, ".csv"),row.names = F)
-  
-  #compute avg performance 
-  final_importance_matrix <- cv_res[[2]]
-  feature_indexes<- which(colnames(model_data) != outcome_colname)
-  features <- colnames(model_data)[feature_indexes]
-  avg_importance_matrix <- compute_avg_importance(final_importance_matrix,features,model_name)
-  write.csv(avg_importance_matrix, paste0(outdir1,"Importance_AVG_", model_name, ".csv"),row.names = F)
-  
-  #Compute perforamnce for each fold with each sampling
-  eachfold_eachSample_perf_tb <- compute_performance_TrainCV_func(N_sampling,NFolds,final_pred)
-  write.csv(eachfold_eachSample_perf_tb, paste0(outdir1,"Performance_PerFoldPerSample_", model_name, ".csv"),row.names = F)
-  
-  #get CI and mean perforamnce
-  CI_perf_tb <- perf_Mean_CI_func(eachfold_eachSample_perf_tb[,3:14])
-  write.csv(CI_perf_tb, paste0(outdir1,"Performance_AVG_CI_", model_name, ".csv"),row.names = T)
-}
-
-####################################################################################### 
-######                           Mortality Prediction   2                  ############
+######                           Mortality Prediction   1                  ############
 #feature file: SOFA.csv, 
 #Outcome file: All_outcome.csv
 ####################################################################################### 
@@ -118,7 +70,7 @@ for (m in 1:length(model_name_list)){
 
 
 ####################################################################################### 
-######                           Mortality Prediction   3                  ############
+######                           Mortality Prediction   2                  ############
 #feature file:  APACHE.csv, 
 #Outcome file: All_outcome.csv
 ####################################################################################### 
@@ -165,6 +117,55 @@ for (m in 1:length(model_name_list)){
   CI_perf_tb <- perf_Mean_CI_func(eachfold_eachSample_perf_tb[,3:14])
   write.csv(CI_perf_tb, paste0(outdir1,"Performance_AVG_CI_", model_name, ".csv"),row.names = T)
 }
+
+####################################################################################### 
+######                           Mortality Prediction   3                   ############
+#feature file: 1. All_Feature_imputed_normed.csv, 
+#Outcome file: All_outcome.csv
+####################################################################################### 
+#1.All_Feature_imputed_normed.csv
+feature_file <- c("All_Feature_imputed_normed_updatedSeptic.csv")
+
+#Outdir for mortality
+outdir1 <- paste0(out_dir,"mortality/AllClinicalFeature/")
+
+#Outcome column name
+outcome_colname <- "Death_inHOSP"
+
+#1.Get model data
+model_data <- construct_model_data_func(data_dir,feature_file,outcome_file,outcome_colname)
+table(model_data$Death_inHOSP)
+colnames(model_data)
+
+#2.CV
+upsample_flag <- 0
+N_sampling <- 10
+NFolds <- 10
+model_name_list <- c("SVM","RF","LogReg","XGB")
+for (m in 1:length(model_name_list)){
+  model_name <- model_name_list[m]
+  #CV
+  cv_res <- cv2_func(model_data,outcome_colname,model_name,upsample_flag,N_sampling,NFolds)
+  final_pred <- cv_res[[1]]
+  write.csv(final_pred, paste0(outdir1,"Prediction_", model_name, ".csv"),row.names = F)
+  
+  #compute avg performance 
+  final_importance_matrix <- cv_res[[2]]
+  feature_indexes<- which(colnames(model_data) != outcome_colname)
+  features <- colnames(model_data)[feature_indexes]
+  avg_importance_matrix <- compute_avg_importance(final_importance_matrix,features,model_name)
+  write.csv(avg_importance_matrix, paste0(outdir1,"Importance_AVG_", model_name, ".csv"),row.names = F)
+  
+  #Compute perforamnce for each fold with each sampling
+  eachfold_eachSample_perf_tb <- compute_performance_TrainCV_func(N_sampling,NFolds,final_pred)
+  write.csv(eachfold_eachSample_perf_tb, paste0(outdir1,"Performance_PerFoldPerSample_", model_name, ".csv"),row.names = F)
+  
+  #get CI and mean perforamnce
+  CI_perf_tb <- perf_Mean_CI_func(eachfold_eachSample_perf_tb[,3:14])
+  write.csv(CI_perf_tb, paste0(outdir1,"Performance_AVG_CI_", model_name, ".csv"),row.names = T)
+}
+
+
 
 
 
