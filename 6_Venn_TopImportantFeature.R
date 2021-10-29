218100a80e7cb8a0894a4d787bae0b6cabcfbfe8
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2021-10-29 11:36:30 -0400

    Update 6_Venn_TopImportantFeature.R

--- 6_Venn_TopImportantFeature.R
+++ 6_Venn_TopImportantFeature.R
@@ -6,7 +6,7 @@ library(ggpolypath)
 source("TAKI_Ultility.R")
 get_important_features_and_cooordinates <-function(perf_dir, select_f_dir, num_top_f){
   #Get all important features matrix
-  method_names <- c("LogReg","RF","SVM","XGB")
+  method_names <- c("Logreg","RF","SVM","XGB")
   imporatnce_list <- list()
   for (i in 1:length(method_names)){
     curr_file <- paste0(perf_dir,"Importance_AVG_",method_names[i],".csv")
@@ -24,7 +24,7 @@ get_important_features_and_cooordinates <-function(perf_dir, select_f_dir, num_t
   selected_final_features_df <- change_listoffeature_name_intable2(selected_final_features_df)
   
   #Get important features for each methods
-  Logreg_fs <- top15_importance_df[,"LogReg_Feature"]
+  LR_fs <- top15_importance_df[,"Logreg_Feature"]
   RF_fs     <- top15_importance_df[,"RF_Feature"]
   SVM_fs    <- top15_importance_df[,"SVM_Feature"]
   XGB_fs    <- top15_importance_df[,"XGB_Feature"]
@@ -37,14 +37,14 @@ get_important_features_and_cooordinates <-function(perf_dir, select_f_dir, num_t
   unique_important_features_df$X <- sample(1:100,nrow(unique_important_features_df))
   unique_important_features_df$Y <- sample(1:100,nrow(unique_important_features_df))
   #Label each feature indicates which method identify each as imporatant
-  unique_important_features_df[which(unique_important_features_df$Features %in% Logreg_fs),"Label_Logreg"] <- "Logreg"
+  unique_important_features_df[which(unique_important_features_df$Features %in% LR_fs),"Label_LR"] <- "LR"
   unique_important_features_df[which(unique_important_features_df$Features %in% RF_fs),"Label_RF"] <- "RF"
   unique_important_features_df[which(unique_important_features_df$Features %in% SVM_fs),"Label_SVM"] <- "SVM"
   unique_important_features_df[which(unique_important_features_df$Features %in% XGB_fs),"Label_XGB"] <- "XGB"
   unique_important_features_df[which(unique_important_features_df$Features %in% Final_Selected_fs),"Label_Selected"] <- "Selected"
   unique_important_features_df$Final_Group <- NA
   for (i in 1:nrow(unique_important_features_df)){
-    curr_labels <- unique_important_features_df[i,c("Label_Logreg","Label_RF","Label_SVM","Label_XGB","Label_Selected")]
+    curr_labels <- unique_important_features_df[i,c("Label_LR","Label_RF","Label_SVM","Label_XGB","Label_Selected")]
     nas_indexes <- which(is.na(curr_labels)==T)
     if(length(nas_indexes) >0){
       curr_labels <- curr_labels[-nas_indexes]
@@ -79,9 +79,9 @@ plot_topfeatures_func<-function(unique_important_features_df,title_name){
           plot.title = element_text(size=20,face="bold")) + 
     theme(panel.border = element_rect(colour = "black", fill=NA, size=2)) + 
     theme(legend.position="bottom",legend.title = element_blank(),legend.text=element_text(size=20)) +
-    scale_colour_manual(breaks = c("Logreg, SVM",
-                                   "Logreg, XGB",
-                                   "Logreg",
+    scale_colour_manual(breaks = c("LR, SVM",
+                                   "LR, XGB",
+                                   "LR",
                                    "RF, SVM, XGB",
                                    "RF, XGB",
                                    "SVM",
@@ -110,13 +110,13 @@ plot_topfeatures_func_MAKE<-function(unique_important_features_df,title_name){
           plot.title = element_text(size=20,face="bold")) + 
     theme(panel.border = element_rect(colour = "black", fill=NA, size=2)) + 
     theme(legend.position="bottom",legend.title = element_blank(),legend.text=element_text(size=20)) +
-    scale_colour_manual(breaks = c("Logreg, XGB",
-                                   "Logreg",
+    scale_colour_manual(breaks = c("LR, XGB",
+                                   "LR",
                                    "RF, XGB",
                                    "SVM",
                                    "Selected",
-                                   "Logreg, RF, SVM, XGB",
-                                   "Logreg, RF",
+                                   "LR, RF, SVM, XGB",
+                                   "LR, RF",
                                    "RF, SVM",
                                    "RF",
                                    "XGB"),
@@ -127,10 +127,19 @@ plot_topfeatures_func_MAKE<-function(unique_important_features_df,title_name){
 
 
 plot_venn_func <- function(set_list){
-  p <- venn(set_list, ilab=FALSE,snames = c("LogReg","Selected","RF","XGBoost","SVM") ,
+  p <- venn(set_list, ilab=FALSE,snames = c("LR","Selected","RF","XGBoost","SVM") ,
             zcolor = c("darkgreen","darkred","darkblue","darkorange","yellow"),ggplot = T,
-            opacity = 0.5,plotsize = 50)
+            opacity = 0.5,plotsize = 100, size = 0)
   return(p)
+}
+
+#change feature name
+change_f_names_func <- function(in_data){
+  in_data <- unique_important_f_df
+  in_data[,"Features"] <- gsub("Vasopressor","Pressor/Inotrope",in_data[,"Features"])
+  in_data[,"Features"] <- gsub("Sodium","Serum Sodium",in_data[,"Features"])
+  
+  return(in_data)
 }
 
 
@@ -142,13 +151,14 @@ outdir   <- paste0(proj_dir,"mortality/")
 
 
 unique_important_f_df <- get_important_features_and_cooordinates(perf_dir,select_f_dir,15)
-mortality_plot <- plot_topfeatures_func(unique_important_f_df,"A.Hospital Mortality")
-png(paste0(outdir,"Mortality_Important_feature_plot.png"),width = 3000,height = 3000,res = 300)
+unique_important_f_df <- change_f_names_func(unique_important_f_df)
+mortality_plot <- plot_topfeatures_func(unique_important_f_df,"Hospital Mortality")
+png(paste0(outdir,"Mortality_Important_feature_plot.png"),width = 3000,height = 3000,res = 120)
 print(mortality_plot)
 dev.off()
 
 #Ven diagram
-Log_reg_fs <- unique_important_f_df$Features[which(is.na(unique_important_f_df[,"Label_Logreg"])==F)]
+Log_reg_fs <- unique_important_f_df$Features[which(is.na(unique_important_f_df[,"Label_LR"])==F)]
 SVM_fs     <- unique_important_f_df$Features[which(is.na(unique_important_f_df[,"Label_RF"])==F)]
 RF_fs      <- unique_important_f_df$Features[which(is.na(unique_important_f_df[,"Label_SVM"])==F)]
 XGB_fs     <- unique_important_f_df$Features[which(is.na(unique_important_f_df[,"Label_XGB"])==F)]
@@ -157,7 +167,7 @@ Selected_fs<- unique_important_f_df$Features[which(is.na(unique_important_f_df[,
 set_list <- list(Log_reg_fs,Selected_fs,RF_fs,XGB_fs,SVM_fs)
 mortality_venn_p <- plot_venn_func(set_list)
 
-png(paste0(outdir,"Mortality_Important_feature_Venn.png"),width = 500,height = 500,res = 120)
+png(paste0(outdir,"Mortality_Important_feature_Venn.png"),width = 1000,height = 500,res = 200)
 print(mortality_venn_p)
 dev.off()
 
@@ -169,13 +179,15 @@ outdir   <- paste0(proj_dir,"make120_drop50/")
 
 
 unique_important_f_df <- get_important_features_and_cooordinates(perf_dir,select_f_dir,15)
-MAKE_plot <- plot_topfeatures_func_MAKE(unique_important_f_df,"B.MAKE")
+unique_important_f_df <- change_f_names_func(unique_important_f_df)
+
+MAKE_plot <- plot_topfeatures_func_MAKE(unique_important_f_df,"MAKE")
 png(paste0(outdir,"MAKE_Important_feature_plot.png"),width = 3000,height = 3000,res = 300)
 print(MAKE_plot)
 dev.off()
 
 #Ven diagram
-Log_reg_fs <- unique_important_f_df$Features[which(is.na(unique_important_f_df[,"Label_Logreg"])==F)]
+Log_reg_fs <- unique_important_f_df$Features[which(is.na(unique_important_f_df[,"Label_LR"])==F)]
 SVM_fs     <- unique_important_f_df$Features[which(is.na(unique_important_f_df[,"Label_RF"])==F)]
 RF_fs      <- unique_important_f_df$Features[which(is.na(unique_important_f_df[,"Label_SVM"])==F)]
 XGB_fs     <- unique_important_f_df$Features[which(is.na(unique_important_f_df[,"Label_XGB"])==F)]
@@ -196,8 +208,8 @@ library(ggpubr)
 outdir <- "/Users/lucasliu/Desktop/DrChen_Projects/All_AKI_Projects/Other_Project/TAKI_Project/Intermediate_Results/Prediction_results0806/Important_feature_Venn_Plots/"
 scatter_plot <- ggarrange(mortality_plot,MAKE_plot, 
                 #labels = c("A", "B"),
-                ncol = 1, nrow = 2)
-png(paste0(outdir,"Mortality_MAKE_scatter.png"),width = 2000,height = 2000,res = 180)
+                ncol = 2, nrow = 1)
+png(paste0(outdir,"Mortality_MAKE_scatter.png"),width = 5000,height = 1500,res = 250)
 print(scatter_plot)
 dev.off()
 
