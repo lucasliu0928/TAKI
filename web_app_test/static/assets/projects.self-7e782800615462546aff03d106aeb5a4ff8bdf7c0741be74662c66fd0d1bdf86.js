faef58e122c689565cafa3df19b2fa9c36488add
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-04-03 20:22:21 -0400

    Delete projects.self-7e782800615462546aff03d106aeb5a4ff8bdf7c0741be74662c66fd0d1bdf86.js

--- web_app_test/static/assets/projects.self-7e782800615462546aff03d106aeb5a4ff8bdf7c0741be74662c66fd0d1bdf86.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  $(document).on("click", ".edit-field-btn", function() {
-    $(this).parent().parent().next().show();
-    return $(this).parent().parent().hide();
-  });
-
-}).call(this);
