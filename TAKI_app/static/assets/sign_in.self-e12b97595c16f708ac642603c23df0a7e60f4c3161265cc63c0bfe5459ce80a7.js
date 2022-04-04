449880812ae920d7f900aa7ec476729d53654eec
JLLucas <39839798+lucasliu0928@users.noreply.github.com>
2022-04-03 23:20:03 -0400

    Create sign_in.self-e12b97595c16f708ac642603c23df0a7e60f4c3161265cc63c0bfe5459ce80a7.js

---
+++ TAKI_app/static/assets/sign_in.self-e12b97595c16f708ac642603c23df0a7e60f4c3161265cc63c0bfe5459ce80a7.js
@@ -0,0 +1,14 @@
+$(document).on('click','.tab a',  function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
